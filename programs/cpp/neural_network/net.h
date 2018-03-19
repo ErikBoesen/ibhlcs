18a7a7e1a56d5ed62e8acb158cd9ea3adb4d5972
ErikBoesen <me@erikboesen.com>
2018-03-18 20:19:03 -0400

    Minor spacing fix in neural network

--- programs/cpp/neural_network/net.h
+++ programs/cpp/neural_network/net.h
@@ -98,7 +98,7 @@ void Neuron::feedForward(const Layer &prevLayer) {
 	// Sum the previous layer's outputs (which are our inputs)
     // Include the bias node from the previous layer.
 
-	for (unsigned n = 0 ; n < prevLayer.size(); ++n) {
+	for (unsigned n = 0; n < prevLayer.size(); ++n) {
 		sum += prevLayer[n].getOutputVal() *
 				 prevLayer[n].outputWeights[this->myIndex].weight;
 	}
