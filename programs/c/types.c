eb46343f26ca95eacf0a3803a9f4409a39b9c570
ErikBoesen <me@erikboesen.com>
2018-04-13 19:20:50 -0400

    Add types.c

---
+++ programs/c/types.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct {
+    char* name;
+    int age;
+    long phone_number;
+} Person;
+
+typedef struct {
+    unsigned int a:1;
+    unsigned int b:2;
+} bits;
+
+enum states {NO, ATTEMPT, YES};
+
+int main() {
+    Person erik = {"Erik Boesen", 17, 0xBAE1E55};
+    printf("%s tiene %d años, y su numero es %ld.\n", erik.name, erik.age, erik.phone_number);
+
+    int a = ATTEMPT;
+
+    bits b = {0, 0};
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", ++b.b);
+    }
+    printf("%d\n", a);
+}
