e1a0f87c712d09d896e695139eb3e957d80e787d
ErikBoesen <me@erikboesen.com>
2018-05-19 14:37:31 -0400

    Basic, broken hangman program

---
+++ programs/cpp/hangman/client.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORT 1885
+#define BUFFER 1
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[BUFFER];
+    if (argc < 2) {
+        std::cout << "usage: " << argv[0] << " [host]" << std::endl;
+        exit(1);
+    }
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cout << "Failed creating socket" << std::endl;
+        exit(1);
+    }
+    if ((server = gethostbyname(argv[1])) < 0) {
+        std::cout << "Failed getting host" << std::endl;
+        exit(1);
+    }
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_port = htons(PORT);
+    if ((connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0) {
+        std::cout << "Failed connecting" << std::endl;
+    }
+    char c;
+    while (1) {
+        std::cout << "Enter a letter: " << std::endl;
+        bzero(buffer, BUFFER);
+        c = std::cin.get();
+        buffer[0] = c;
+        write(sockfd, buffer, BUFFER);
+        bzero(buffer, BUFFER);
+        read(sockfd, buffer, BUFFER);
+        std::cout << buffer << std::endl;
+    }
+}
---
+++ programs/cpp/hangman/server.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFFER 1
+#define PORT 1885
+#define CHANCES 10
+
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, port;
+    socklen_t clilen;
+    char buffer[BUFFER];
+    struct sockaddr_in serv_addr, cli_addr;
+    if (argc < 2) {
+        std::cout << "usage: " << argv[0] << " [solution]" << std::endl;
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+    bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    int chances = CHANCES;
+    while (chances > 0) {
+        bzero(buffer, BUFFER);
+        read(newsockfd, buffer, BUFFER);
+        std::cout << "Message: " << buffer << std::endl;
+        char response[] = "Message received";
+        write(newsockfd, response, sizeof(response));
+        chances--;
+    }
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
