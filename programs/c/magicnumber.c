fe88e09cccedcec223b7c01f55dcec7ec5f988ee
ErikBoesen <me@erikboesen.com>
2018-06-16 19:01:58 -0400

    Some more broken updates to magicnumber.c

--- programs/c/magicnumber.c
+++ programs/c/magicnumber.c
@@ -1,9 +1,12 @@
 // Find first number which, when you take the largest digit and place it at the end, is half the original number.
+// BROKEN
 #include <stdio.h>
 #include <math.h>
 #include <inttypes.h>
 
+#define DEBUG
 #define BASE 10
+#define BASETENANS 105263157894736842
 
 uint8_t numdigits(uint64_t x) {
     uint8_t count;
@@ -19,19 +22,34 @@ int digof(uint64_t x, uint8_t n) { return (uint64_t)(x % (int)pow(BASE, n+1)) / 
 uint64_t convert(uint64_t x) {
     int n = numdigits(x);
     int nth = digof(x, n - 1);
+#ifdef DEBUG
+    printf("n/nth: %d %d\n", n, nth);
+#endif
+#ifdef DEBUG
+    printf("%llu converts to ", x);
+#endif
     x -= pow(BASE, n - 1) * nth;
     x *= BASE;
     x += nth;
+#ifdef DEBUG
+    printf("%llu\n", x);
+#endif
     return x;
 }
 
 int works(uint64_t x) {
-    return (2 * convert(x) == x);
+    return ((2 * convert(x)) == x);
 }
 
 int main(int argc, char *argv[]) {
-    uint64_t x = 1;
-    for (; !works(x); ++x);
-    printf("%llu\n", x);
+    printf("%d\n", numdigits(105263157894736842ull));
+    printf("%llu %llu\n", 105263157894736842ull, convert(105263157894736842));
+
+    /*uint64_t x = BASETENANS - 10;
+    for (; !works(x); ++x) {
+        //printf("Trying %llu\n", x);
+        if (x > BASETENANS + 30) break;
+    }
+    printf("%llu\n", x);*/
     return 0;
 }
