04c1b4fa00369b7b1b59ec1e2d72eea5cd14a0f7
ErikBoesen <me@erikboesen.com>
2018-09-07 19:16:37 -0400

    Fix autocards.py

--- programs/py/autocards.py
+++ programs/py/autocards.py
@@ -5,31 +5,35 @@ losses = 0
 
 game = 0
 while True:
+    game += 1
+    # TODO: Seems unnecessary to use this variable.
+    victory = True
     print('-' * 2 + f' Game {game} ' + '-' * 15)
     cards = list(range(2, 10+1))
     old = random.choice(cards)
     while len(cards) > 1:
-        new = random.choice(cards)
         print(cards)
         print(f'Card: {old}')
-        print(f'Next: {new}')
         print(f'Position in list: {cards.index(old) + 1}/{len(cards)}')
-        guess = (cards.index(old) + 1 < len(cards) / 2)
+        guess = (cards.index(old) + 1 <= len(cards) // 2)
         print('Guess: ' + ('higher' if guess else 'lower'))
         cards.remove(old)
+        new = random.choice(cards)
+        print(f'Next: {new}')
         if guess and new > old or (not guess) and new < old:
             print('Correct.')
             old = new
         else:
             print('Incorrect.')
             losses += 1
+            victory = False
             break
-    if not cards:
+    if victory:
         print('You win!')
         wins += 1
-
-    game += 1
     print(f'Wins:   {wins}')
     print(f'Losses: {losses}')
     print(f'Record: {100 * (wins / (wins+losses))}')
     print()
+
+
