b2876df95fe3a5c15a86b303d67848001fa09444
ErikBoesen <me@erikboesen.com>
2018-11-08 09:17:38 -0500

    Scale up canvas to whole window

--- programs/web/canvas/pong.js
+++ programs/web/canvas/pong.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('canv');
+canvas.height = window.innerHeight;
+canvas.width  = window.innerWidth;
 var ctx = canvas.getContext('2d');
-
 function rand(x) {
      return (Math.floor(Math.random() * 256));
 }
