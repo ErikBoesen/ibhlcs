5f7de3c99a43220b445cc08668c04f2557822b11
ErikBoesen <me@erikboesen.com>
2018-11-12 09:29:21 -0500

    Draw sphere for three

--- programs/web/three/scripts.js
+++ programs/web/three/scripts.js
@@ -1,6 +1,6 @@
 // Set the scene size.
-const WIDTH = 400;
-const HEIGHT = 300;
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
 
 // Set some camera attributes.
 const VIEW_ANGLE = 45;
@@ -33,3 +33,55 @@ renderer.setSize(WIDTH, HEIGHT);
 // Attach the renderer-supplied
 // DOM element.
 container.appendChild(renderer.domElement);
+
+const RADIUS = 50;
+const SEGMENTS = 16;
+const RINGS = 16;
+
+// create the sphere's material
+const sphereMaterial =
+  new THREE.MeshLambertMaterial(
+    {
+      color: 0xCC0000
+    });
+
+// Create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+const sphere = new THREE.Mesh(
+
+  new THREE.SphereGeometry(
+    RADIUS,
+    SEGMENTS,
+    RINGS),
+
+  sphereMaterial);
+
+// Move the Sphere back in Z so we
+// can see it.
+sphere.position.z = -300;
+
+// Finally, add the sphere to the scene.
+scene.add(sphere);
+
+// create a point light
+const pointLight =
+  new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+// add to the scene
+scene.add(pointLight);
+
+function animate() {
+  // Schedule the next frame.
+  requestAnimationFrame(animate);
+
+  // Draw!
+  renderer.render(scene, camera);
+}
+
+animate();
