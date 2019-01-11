59330dcd4b1fa2df4b51c5b275eacda19ae4ce25
ErikBoesen <me@erikboesen.com>
2019-01-11 09:24:30 -0500

    Add queue python program

---
+++ programs/py/queue.py
@@ -0,0 +1,18 @@
+
+
+class Queue:
+    items = []
+    def __len__(self):
+        return len(self.items)
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        return self.items.pop(0)
+
+q = Queue()
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+print(q.dequeue())
+print(len(q))
