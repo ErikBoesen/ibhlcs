4747439fb3cf47948f771cefb5418cfd79bb1cf4
ErikBoesen <me@erikboesen.com>
2019-01-15 10:40:35 -0500

    Only accept prime numbers in queue.py

--- programs/py/queue.py
+++ programs/py/queue.py
@@ -1,16 +1,26 @@
+import math
+
 class Queue:
     items = []
     def __len__(self):
         return len(self.items)
+    def is_prime(self, x: int):
+        for i in range(2, int(math.sqrt(x)) + 1):
+            if x % i == 0:
+                return False
+        return True
     def enqueue(self, item):
-        self.items.append(item)
+        if self.is_prime(item):
+            self.items.append(item)
+            return True
+        return False
     def dequeue(self):
         return self.items.pop(0)
 
 q = Queue()
-q.enqueue(3)
-q.enqueue(4)
-q.enqueue(5)
+print(q.enqueue(3))
+print(q.enqueue(4))
+print(q.enqueue(5))
 
 print(q.dequeue())
 print(len(q))
