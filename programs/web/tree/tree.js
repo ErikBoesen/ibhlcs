8def1d223e2e4a487c944a8d376520381edb9a52
ErikBoesen <me@erikboesen.com>
2019-01-29 12:44:33 -0500

    Implement automatic option setting

--- programs/web/tree/tree.js
+++ programs/web/tree/tree.js
@@ -4,33 +4,59 @@ canvas.width  = window.innerWidth;
 var ctx = canvas.getContext('2d');
 
 var options = {
-    sideLengthMultiplier: .75,
-    middleLengthMultiplier: .5,
+    sideLengthMultiplier: {
+        default: .75,
+        min: 0,
+        max: 1,
+    },
+    middleLengthMultiplier: {
+        default: .5,
+        min: 0,
+        max: 0,
+    },
 };
+
+const CONTROLS = document.getElementById('controls');
+
+for (option in options) {
+    slider = document.createElement('input');
+    options[option].readout = document.createElement('label');
+    slider.type = 'range';
+    slider.min = options[option].min;
+    slider.max = options[option].max;
+    slider.value = options[option].default;
+    slider.id = option;
+    slider.step = 0.05;
+    options[option].slider = slider;
+    CONTROLS.appendChild(slider);
+}
 
 ctx.strokeStyle = 'white';
 function random() {
-    return 0.2;
     return Math.random() / 8;
 }
 var height = window.innerHeight;
 function drawBranch(iterations, length, startX, startY, angle) {
-    if (iterations == 0) return;
     ctx.moveTo(startX, height - startY);
     var endX = startX + Math.cos(angle) * length;
     var endY = startY + Math.sin(angle) * length;
     ctx.lineTo(endX, height - endY);
-    drawBranch(iterations - 1, length * options.sideLengthMultiplier, endX, endY, angle - Math.PI / 4 + random());
-    drawBranch(iterations - 1, length * options.middleLengthMultiplier, endX, endY, angle + 0 + random());
-    drawBranch(iterations - 1, length * options.sideLengthMultiplier, endX, endY, angle + Math.PI / 4 + random());
+    if (iterations > 0) {
+        drawBranch(iterations - 1, length * options.sideLengthMultiplier.value, endX, endY, angle - Math.PI / 4 + random());
+        drawBranch(iterations - 1, length * options.middleLengthMultiplier.value, endX, endY, angle + 0 + random());
+        drawBranch(iterations - 1, length * options.sideLengthMultiplier.value, endX, endY, angle + Math.PI / 4 + random());
+    }
 }
 
 function startTree() {
     ctx.beginPath();
-    drawBranch(8, 100, 500, 0, Math.PI / 2);
+    drawBranch(8, 100, window.innerWidth / 2, 0, Math.PI / 2);
     ctx.stroke();
 }
 
-startTree();
-document.setInterval(100, startTree());
+setInterval(100, startTree());
 
+oninput = function(e) {
+    console.log(e.target);
+    options[e.target.id].value = e.target.value;
+}
