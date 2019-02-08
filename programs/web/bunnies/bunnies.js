92524368eb85563f261e971ce880382d5d0a949e
ErikBoesen <me@erikboesen.com>
2019-02-08 09:14:33 -0500

    Add bunny drawing logic

--- programs/web/bunnies/bunnies.js
+++ programs/web/bunnies/bunnies.js
@@ -2,6 +2,8 @@ let canvas = document.getElementById('canv');
 const RES = 32;
 const SIZE = 20;
 const MAX_GRASS_GROWTH = 10;
+const STARTING_BUNNY_COUNT = 4;
+const BUNNY_PADDING = 3;
 canvas.height = RES * SIZE;
 canvas.width  = RES * SIZE;
 let ctx = canvas.getContext('2d');
@@ -15,8 +17,16 @@ for (row = 0; row < RES; row++) {
         grass[row].push(MAX_GRASS_GROWTH);
     }
 }
-console.log(grass);
+function random() {
+    return Math.floor(Math.random() * RES);
+}
 let bunnies = [];
+for (count = 0; count < STARTING_BUNNY_COUNT; count++) {
+    bunnies.push({
+        'x': random(),
+        'y': random(),
+    });
+}
 let wolves = [];
 
 function tick() {
@@ -28,6 +38,10 @@ function draw() {
             ctx.fillStyle = 'rgb(0,' + (255/10 * grass[row][col]) + ',0)';
             ctx.fillRect(col * SIZE, row * SIZE, SIZE, SIZE);
         }
+    }
+    ctx.fillStyle = '#333';
+    for (bunny of bunnies) {
+        ctx.fillRect(col * SIZE + BUNNY_PADDING, row * SIZE + BUNNY_PADDING, SIZE - 2*BUNNY_PADDING, SIZE - 2*BUNNY_PADDING);
     }
 }
 function loop() {
