c7003dcca1d695993005532f6f8e689f660f8ddc
ErikBoesen <me@erikboesen.com>
2019-02-08 08:54:36 -0500

    Clean up irrelevant code

--- programs/web/bunnies/bunnies.js
+++ programs/web/bunnies/bunnies.js
@@ -1,16 +1,8 @@
 var canvas = document.getElementById('canv');
-canvas.height = window.innerHeight;
-canvas.width  = window.innerWidth;
+const RES = 64;
+const SIZE = 20;
+canvas.height = RES * SIZE;
+canvas.width  = RES * SIZE;
 var ctx = canvas.getContext('2d');
 
-var xSpeed = 1,
-    ySpeed = 1;
 
-function rand(x) {
-     return (Math.floor(Math.random() * 256));
-}
-var size = 100;
-for (i = 0; i < 20; i++) {
-    ctx.fillStyle = 'rgb(' + rand(256) + ',' + rand(256) + ',' + rand(256) + ')';
-    ctx.fillRect(rand(canvas.width - size), rand(canvas.height - size), rand(size), rand(size));
-}
