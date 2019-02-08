d21593c4f360142701a0a8548c1b384ba89b4a91
ErikBoesen <me@erikboesen.com>
2019-02-08 09:07:46 -0500

    Add grass drawing and main loop for bunnies

--- programs/web/bunnies/bunnies.js
+++ programs/web/bunnies/bunnies.js
@@ -1,20 +1,38 @@
-var canvas = document.getElementById('canv');
+let canvas = document.getElementById('canv');
 const RES = 32;
 const SIZE = 20;
 const MAX_GRASS_GROWTH = 10;
 canvas.height = RES * SIZE;
 canvas.width  = RES * SIZE;
-var ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 
-var grass = [];
+console.log('Started run.');
+
+let grass = [];
 for (row = 0; row < RES; row++) {
     grass.push([]);
     for (col = 0; col < RES; col++) {
         grass[row].push(MAX_GRASS_GROWTH);
     }
 }
+console.log(grass);
+let bunnies = [];
+let wolves = [];
 
+function tick() {
+
+}
+function draw() {
+    for (row = 0; row < RES; row++) {
+        for (col = 0; col < RES; col++) {
+            ctx.fillStyle = 'rgb(255,' + 255/10 * grass[row][col] + '255)';
+            ctx.fillRect(col * SIZE, row * SIZE, SIZE, SIZE);
+        }
+    }
+}
 function loop() {
     tick();
     draw();
 }
+
+let main_loop = setInterval(loop, 100);
