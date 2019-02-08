afcd9ed2365116630f159e6866f8a8a6c8876db1
ErikBoesen <me@erikboesen.com>
2019-02-08 09:02:29 -0500

    Add grass growth support

--- programs/web/bunnies/bunnies.js
+++ programs/web/bunnies/bunnies.js
@@ -1,8 +1,20 @@
 var canvas = document.getElementById('canv');
-const RES = 64;
+const RES = 32;
 const SIZE = 20;
+const MAX_GRASS_GROWTH = 10;
 canvas.height = RES * SIZE;
 canvas.width  = RES * SIZE;
 var ctx = canvas.getContext('2d');
 
+var grass = [];
+for (row = 0; row < RES; row++) {
+    grass.push([]);
+    for (col = 0; col < RES; col++) {
+        grass[row].push(MAX_GRASS_GROWTH);
+    }
+}
 
+function loop() {
+    tick();
+    draw();
+}
