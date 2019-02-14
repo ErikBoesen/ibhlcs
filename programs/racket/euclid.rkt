bbd5fbc024babbb69d04e3877f828679b5cf8f8c
ErikBoesen <me@erikboesen.com>
2019-02-14 12:51:57 -0500

    Use unless for euclid

--- programs/racket/euclid.rkt
+++ programs/racket/euclid.rkt
@@ -4,7 +4,7 @@
   (define r (modulo a b))
   (define multiplier (/ (- a r) b))
   (fprintf (current-output-port) "~s = ~s * ~s + ~s\n" a b multiplier r)
-  (when (not (eq? r 0))
+  (unless (eq? r 0)
     (euclid b r)))
 
 (euclid 45 25)
