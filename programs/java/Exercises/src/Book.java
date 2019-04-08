f7c645b585a26e934a55a92e1f44582c355ac93a
ErikBoesen <me@erikboesen.com>
2019-04-08 09:06:59 -0400

    Use HashMap for books

--- programs/java/Exercises/src/Book.java
+++ programs/java/Exercises/src/Book.java
@@ -1,16 +1,12 @@
 public class Book {
-    String title;
     double fine;
     boolean availability;
-    public Book(String title, double fine, boolean availability) {
-        this.setTitle(title);
+    public Book(double fine, boolean availability) {
         this.setFine(fine);
         this.setAvailability(availability);
     }
     public double getFine() { return this.fine; }
     public void setFine(double fine) { this.fine = fine; }
-    public String getTitle() { return this.title; }
-    public void setTitle(String title) { this.title = title; }
     public boolean getAvailability() { return this.availability; }
     public void setAvailability(boolean availability) { this.availability = availability; }
 }
--- programs/java/Exercises/src/BookDriver.java
+++ programs/java/Exercises/src/BookDriver.java
@@ -1,23 +1,28 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BookDriver {
     public static void main(String[] args) {
-        ArrayList<Book> books = new ArrayList<Book>();
-        books.add(new Book("How to get into MIT", 3.14, true));
-        books.add(new Book("How to play tennis", 2.13, false));
-        books.add(new Book("How to win friends and influence people", 4.93, true));
+        HashMap<String, Book> books = new HashMap<>();
+        books.put("How to get into MIT", new Book(3.14, true));
+        books.put("How to play tennis", new Book(2.13, false));
+        books.put("How to win friends and influence people", new Book(4.93, true));
         Scanner kb = new Scanner(System.in);
         System.out.print("Title: ");
         String title = kb.nextLine();
         System.out.print("Fine: ");
         double fine = kb.nextDouble();
         System.out.print("Available? (y/n) ");
-        boolean availability = kb.next().equalsIgnoreCase("y");
-        books.add(new Book(title, fine, availability));
+        boolean availability = Character.toLowerCase(kb.next().charAt(0)) == 'y';
+        books.put(title, new Book(fine, availability));
 
         while (true) {
-            String 
+            System.out.print("What book would you like to check out?");
+            String title = kb.nextLine();
+            if (books.containsKey(title)) {
+                
+            } else {
+                System.out.println("Sorry, we don't have that book.");
+            }
         }
     }
 }
